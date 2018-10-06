cedf0c50b8b5c2e739be4eb1ef2eff54f783d6e5
Mark Escobedo <mark@keystoke.com>
2018-10-05 20:36:08 -0700

    Added some redux things

---
+++ actions/restaurantActions.js
@@ -0,0 +1,9 @@
+export function loadRestaurantsSuccess(restaurants) {
+  return { type: 'LOAD_RESTAURANTS_SUCCESS', restaurants };
+}
+
+export function searchRestaurants(options, callback) {
+  return (dispatch) => {
+
+  };
+}
