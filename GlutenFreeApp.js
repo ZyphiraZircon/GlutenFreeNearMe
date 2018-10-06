8ad694e093c6a9c91f61c8fa6de771fb76061839
Mark Escobedo <mark@keystoke.com>
2018-10-05 20:58:12 -0700

    Navigation, nativebase, and some test screens

--- GlutenFreeApp.js
+++ GlutenFreeApp.js
@@ -1,13 +1,24 @@
 import React, {Component} from 'react';
 import { Text, View } from 'react-native';
+import { StackNavigator } from 'react-navigation';
+import SearchScreen from './components/SearchScreen';
+import ResultsScreen from './components/ResultsScreen';
+
+const RootRouter = StackNavigator(
+  {
+    Search: { screen: SearchScreen },
+    Results: { screen: ResultsScreen }
+  },
+  {
+    headerMode: 'none'
+  }
+);
 
 
 export default class App extends Component {
   render() {
     return (
-      <View>
-        <Text>Hello</Text>
-      </View>
+      <RootRouter />
     );
   }
 }
